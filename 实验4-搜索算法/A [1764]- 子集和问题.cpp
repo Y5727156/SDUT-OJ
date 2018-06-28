434b6698cb37582506035d01d6bd2090f5e51a22
Young <2732842075@qq.com>
2018-06-28 11:20:13 +0800

    Modified

--- 实验4-搜索算法/A [1764]- 子集和问题.cpp
+++ 实验4-搜索算法/A [1764]- 子集和问题.cpp
@@ -1,62 +1,39 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<cstdlib>
+#include  <bits/stdc++.h>
 using namespace std;
-int n,c,a[10000],b[10000],d[10000],i,sum=0;
-bool pd;
-
-void print(int dep)
+int n,c,a[20000],b[20000],x[20000],flag;
+void dfs(int s,int sum)
 {
-	int j;
-	for (j=1;j<=dep;++j)
-	  printf("%d ",d[j]);
-	printf("\n");
-	return;
-}
-
-void dfs(int dep)
-{
-	int r;
-	if (pd==1) return;//因为只需输出一组解，c++又没法结束整个程序，，怕超时到处加判断。。。（我智商低别理我）
-	for (r=1;r<=n;++r)
-	  if (!b[r]&&c-a[r]>=0)
-	  {
-	  	if (pd==1) return;
-	  	c-=a[r];//用c减去每个数，其实就是几个数之和等于c
-	  	d[dep]=a[r];//d是记录数组
-	  	b[r]=1;
-	  	if (c==0) //如果找到一组解，直接输出，完成任务
-	  	{
-	  		if (pd==0)
-	  		  print(dep);
-	  		pd=1;
-	  		return;
-	  	}
-	  	else dfs(dep+1);
-	  	if (pd==1) return;
-	  	c+=a[r];//回溯一步
-	  	b[r]=0;
-	  }
+	if(sum>c)
+        return;
+	if(sum==c){
+		for(int i=0;i<n;i++)
+		if(b[i]){
+			if(!flag){
+				printf("%d",a[i]);
+				flag=1;
+			}
+			else
+			printf(" %d",a[i]);
+		}
+		return;
+	}
+	for(int i=s;i<n&&!flag;i++)
+	if(sum+x[i]>=c){
+		b[i]=1;
+		dfs(i+1,sum+a[i]);
+		b[i]=0;
+	}
 }
 int main()
 {
-	scanf("%d%d",&n,&c);
-	for (i=1;i<=n;++i)
-	{
-		scanf("%d",&a[i]);
-		sum+=a[i];
-	}
-	if (sum<c)//很重要的剪枝！！如果所有的数加起来都小于c，那么不可能有解。。之前有三组TLE，加了这一步竟然给蒙过了。。
-	{
+	cin>>n>>c;
+	for(int i=0;i<n;i++)
+		cin>>a[i];
+	for(int i=n-1;i>=0;i--)
+		x[i]=a[i]+x[i+1];
+	dfs(0,0);
+	if(!flag)
 		printf("No Solution!");
-		return 0;
-	}
-	memset(d,0,sizeof(d));
-	memset(b,0,sizeof(b));
-	pd=0;
-	dfs(1);
-	if (!pd) printf("No Solution!");
 	return 0;
 }
 
