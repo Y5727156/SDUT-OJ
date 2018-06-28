c422c7eedd92c622bb1e51961c776a1b6d1d95c2
Young <2732842075@qq.com>
2018-06-28 18:36:59 +0800

    Modified

--- 实验4-搜索算法/D[1771] - 整数变换问题.cpp
+++ 实验4-搜索算法/D[1771] - 整数变换问题.cpp
@@ -1,39 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct node 
-{
-int s, x, f, p;
+struct node{
+    int s, x, f, p;
 };
 int n, m;
-map<int, node> mmp;
+map<int, node> mp;
 queue<node> q;
 int main() {
     cin >> n >> m;
     node a = {0, n, 0, -1};
-    mmp[n] = a;
+    mp[n] = a;
     q.push(a);
     while (q.size()) {
-        node a = q.front();
+        a = q.front();
         q.pop();
         if (a.x == m) {
             cout << a.s << endl;
             while (~a.p) {
                 cout << (a.f ? 'f' : 'g');
-                a = mmp[a.p];
+                a = mp[a.p];
             }
             cout << endl;
             break;
         }
         node b = a;
         b.s++, b.x = a.x * 3, b.f = 1, b.p = a.x;
-        if (!mmp.count(b.x)) {
-            mmp[b.x] = b;
+        if (!mp.count(b.x)) {
+            mp[b.x] = b;
             q.push(b);
         }
         b = a;
         b.s++, b.x = a.x / 2, b.f = 0, b.p = a.x;
-        if (!mmp.count(b.x)) {
-            mmp[b.x] = b;
+        if (!mp.count(b.x)) {
+            mp[b.x] = b;
             q.push(b);
         }
     }
