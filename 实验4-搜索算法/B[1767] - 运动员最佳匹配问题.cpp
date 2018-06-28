745f14d31fe2e2f15d581263d96e81c6063c8da7
Young <2732842075@qq.com>
2018-06-28 08:13:33 +0800

    Update

--- 实验4-搜索算法/B[1767] - 运动员最佳匹配问题.cpp
+++ 实验4-搜索算法/B[1767] - 运动员最佳匹配问题.cpp
@@ -1,65 +1,41 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<cstdlib>
+#include<bits/stdc++.h>
 using namespace std;
-int n,c,a[10000],b[10000],d[10000],i,sum=0;
-bool pd;
-
-void print(int dep)
+int p[25][25],q[25][25],n,c[25],ans;
+bool vis[25];
+void dfs(int x,int tot)
 {
-	int j;
-	for (j=1;j<=dep;++j)
-	  printf("%d ",d[j]);
-	printf("\n");
-	return;
+    if(x>n){
+        ans=max(ans,tot);
+        return;
+    }
+    int op=0;
+    for(int i=x;i<=n;i++)
+        op+=c[i];
+    if(ans>op+tot)
+        return;
+    for(int i=1;i<=n;i++)
+        if(!vis[i]){
+            vis[i]=1;
+            dfs(x+1,tot+p[x][i]*q[i][x]);
+            vis[i]=0;
+        }
 }
-
-void dfs(int dep)
-{
-	int r;
-	if (pd==1) return;//因为只需输出一组解，c++又没法结束整个程序，，怕超时到处加判断。。。（我智商低别理我）
-	for (r=1;r<=n;++r)
-	  if (!b[r]&&c-a[r]>=0)
-	  {
-	  	if (pd==1) return;
-	  	c-=a[r];//用c减去每个数，其实就是几个数之和等于c
-	  	d[dep]=a[r];//d是记录数组
-	  	b[r]=1;
-	  	if (c==0) //如果找到一组解，直接输出，完成任务
-	  	{
-	  		if (pd==0)
-	  		  print(dep);
-	  		pd=1;
-	  		return;
-	  	}
-	  	else dfs(dep+1);
-	  	if (pd==1) return;
-	  	c+=a[r];//回溯一步
-	  	b[r]=0;
-	  }
-}
-
 int main()
 {
-	scanf("%d%d",&n,&c);
-	for (i=1;i<=n;++i)
-	{
-		scanf("%d",&a[i]);
-		sum+=a[i];
-	}
-	if (sum<c)//很重要的剪枝！！如果所有的数加起来都小于c，那么不可能有解。。之前有三组TLE，加了这一步竟然给蒙过了。。
-	{
-		printf("No Solution!");
-		return 0;
-	}
-	memset(d,0,sizeof(d));
-	memset(b,0,sizeof(b));
-	pd=0;
-	dfs(1);
-	if (!pd) printf("No Solution!");
-	return 0;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=n;j++)
+            cin>>p[i][j];
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=n;j++)
+            cin>>q[i][j];
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=n;j++)c[i]=max(c[i],p[i][j]*q[j][i]);
+    dfs(1,0);
+    cout<<ans;
+    return 0;
 }
+
 
 Sample Input
 
